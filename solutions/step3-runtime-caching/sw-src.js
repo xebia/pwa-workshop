4c227a75d7ff22feddbad25dbedce80d097844f3
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-05-24 16:34:42 +0200

    Cache news at runtime

--- solutions/step3-runtime-caching/sw-src.js
+++ solutions/step3-runtime-caching/sw-src.js
@@ -1,3 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute([]);
+
+workbox.routing.registerRoute(
+  '/news',
+  workbox.strategies.networkFirst()
+);
