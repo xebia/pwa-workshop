9d4ab0b8e7d4c21849e4fe7bf5d49c7143393ea3
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-05-24 16:24:25 +0200

    Add basic error handling

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 fetch('/news')
-  .then(res => res.json())
+  .then(
+    res => res.json(),
+    () => {
+      const newsList = document.getElementById('news-list');
+      newsList.outerHTML = 'Could not load news';
+    }
+  )
   .then(news => {
     const newsList = document.getElementById('news-list');
     newsList.innerHTML = news
