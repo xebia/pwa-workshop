9de51a4bdaf43f59df9abdf1d2a0a119febd568c
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-05-27 14:54:55 +0200

    Add changing-news to servers

--- solutions/step2-workbox-precache/server.js
+++ solutions/step2-workbox-precache/server.js
@@ -2,11 +2,29 @@ const Koa = require('koa');
 const koaStatic = require('koa-static');
 const koaProxies = require('koa-proxies');
 
+const enable_changing = false;
+
 const app = new Koa();
 
 const port = 8080;
 
 app.use(koaStatic('.'));
+
+let i = 0;
+const titles = ['Some news title', 'Another news title', 'Random news event'];
+app.use((ctx, next) => {
+  if (ctx.path === '/news' && enable_changing) {
+    ctx.body = [{
+      title: titles[i],
+      time_ago: Date.now()
+    }];
+    i = (i + 1) % titles.length;
+
+    ctx.response.lastModified = new Date();
+  } else {
+    return next();
+  }
+});
 
 app.use(koaProxies('/', {
   target: 'https://api.hackerwebapp.com/',
